762a56664aea8724d431d90620605fb9b2d1452e
Tetsuya <tw5522@icloud.com>
2024-01-08 19:24:31 +0900

    Modified create_lst.py by making class and using argparse

--- PIDNet/tools/create_lst.py
+++ PIDNet/tools/create_lst.py
@@ -1,21 +1,41 @@
 import os
+import argparse
 
-directory = "./data/blood_vessel_segmentation/val"
-output_file = "./data/list/blood_vessel_seg/val.lst"
-no_need_str = "./data/blood_vessel_segmentation/"
 
-with open(output_file, "w") as file:
-    for root, dirs, files in os.walk(directory):
-        if "images" not in root:
-            continue
-        files.sort()  # Sort files in ascending order
-        for filename in files:
-            if filename.endswith(".tif"):
-                # file.write(os.path.join(root, filename) + "\n")
-                file_path = os.path.join(root, filename)
-                out_img_str = file_path.replace(no_need_str, "")
-                out_label_str = out_img_str.replace("images", "labels")
-                if os.path.exists(os.path.join(no_need_str, out_label_str)):
-                    file.write(out_img_str + "\t" + out_label_str + "\n")
-                else:
-                    file.write(out_img_str + "\n")
+class CreateList:
+    def __init__(self, directory, output_file, no_need_str):
+        self.directory = directory
+        self.output_file = output_file
+        self.no_need_str = no_need_str
+
+    def create_lst_file(self):
+        with open(self.output_file, "w") as file:
+            for root, dirs, files in os.walk(self.directory):
+                if "images" not in root:
+                    continue
+                files.sort()  # Sort files in ascending order
+                for filename in files:
+                    if filename.endswith(".tif"):
+                        file_path = os.path.join(root, filename)
+                        out_img_str = file_path.replace(self.no_need_str, "")
+                        out_label_str = out_img_str.replace("images", "labels")
+                        if os.path.exists(os.path.join(self.no_need_str, out_label_str)):
+                            file.write(out_img_str + "\t" +
+                                       out_label_str + "\n")
+                        else:
+                            file.write(out_img_str + "\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create list file")
+    parser.add_argument("--directory", type=str, help="Label directory path",
+                        default="data/blood_vessel_segmentation/train")
+    parser.add_argument("--output_file", type=str, help="Output lst file path",
+                        default="data/list/blood_vessel_seg/train.lst")
+    parser.add_argument("--no_need_str", type=str,
+                        help="String to be replaced", default="data/blood_vessel_segmentation/")
+    args = parser.parse_args()
+
+    create_list = CreateList(
+        args.directory, args.output_file, args.no_need_str)
+    create_list.create_lst_file()
