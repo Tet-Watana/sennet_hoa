6b9e5bfebce6ec00f015dc44b15507a5e59278b9
Tetsuya <tw5522@icloud.com>
2024-01-08 18:06:22 +0900

    Created class in resize_images.py

--- PIDNet/tools/resize_images.py
+++ PIDNet/tools/resize_images.py
@@ -1,57 +1,81 @@
 import os
 import cv2
 import multiprocessing
-
-# Define the directories
-train_dir = "data/blood_vessel_segmentation/train"
-val_dir = "data/blood_vessel_segmentation/val"
-
-# Define the target size
-target_size = (1512, 1704)
-
-# Function to resize and save a single image
+import argparse
 
 
-def resize_and_save_image(image_path):
-    # Open the image
-    image = cv2.imread(image_path)
+class ImageResizer:
+    def __init__(self, target_size, output_dir):
+        self.target_size = target_size
+        self.output_dir = output_dir
 
-    # Resize the image
-    resized_image = cv2.resize(image, target_size)
+    def resize_and_save_image(self, image_path):
+        # Open the image
+        image = cv2.imread(image_path)
 
-    # Check if "label" is in image_path
-    if "label" in image_path:
-        # Convert resized_image to binary image
-        _, resized_image = cv2.threshold(
-            resized_image, 127, 255, cv2.THRESH_BINARY)
+        # Resize the image
+        resized_image = cv2.resize(image, self.target_size)
 
-    # Save the resized image
-    cv2.imwrite(image_path, resized_image)
+        # Check if "label" is in image_path
+        if "label" in image_path:
+            # Convert resized_image to binary image
+            _, resized_image = cv2.threshold(
+                resized_image, 127, 255, cv2.THRESH_BINARY)
 
-# Function to process images in parallel
+        parent_dir = os.path.basename(
+            os.path.dirname(image_path))  # images or labels
+        gparent_dir = os.path.basename(os.path.dirname(
+            os.path.dirname(image_path)))  # kidney1_dense,
+        ggparent_dir = os.path.basename(os.path.dirname(
+            os.path.dirname(os.path.dirname(image_path))))  # train, val
+        # Get the output image path
+        output_image_path = os.path.join(
+            self.output_dir, ggparent_dir, gparent_dir, parent_dir, os.path.basename(image_path))
+        # print(output_image_path)
+        os.makedirs(os.path.dirname(output_image_path), exist_ok=True)
+        # Save the resized image
+        cv2.imwrite(output_image_path, resized_image)
+
+    def process_images(self, directory):
+        # Create a pool of worker processes
+        pool = multiprocessing.Pool()
+
+        # Iterate over the root, directories, and files in the directory
+        for root, directories, files in os.walk(directory):
+            # Iterate over the files
+            for file in files:
+                # Check if the file is an image
+                if file.endswith(".tif"):
+                    # Process the image in parallel
+                    image_path = os.path.join(root, file)
+                    pool.apply_async(self.resize_and_save_image, (image_path,))
+
+        # Close the pool and wait for all processes to finish
+        pool.close()
+        pool.join()
 
 
-def process_images(directory):
-    # Create a pool of worker processes
-    pool = multiprocessing.Pool()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Resize images")
+    parser.add_argument("--train_dir", type=str, default="data/blood_vessel_segmentation_original/train",
+                        help="Directory containing training images")
+    parser.add_argument("--val_dir", type=str, default="data/blood_vessel_segmentation_original/val",
+                        help="Directory containing validation images")
+    parser.add_argument("--width", type=int, default=1512,
+                        help="Target width of the resized images")
+    parser.add_argument("--height", type=int, default=1704,
+                        help="Target height of the resized images")
+    parser.add_argument("--output_dir", type=str, default="data/blood_vessel_segmentation/",
+                        help="Directory to save the resized images")
 
-    # Iterate over the root, directories, and files in the directory
-    for root, directories, files in os.walk(directory):
-        # Iterate over the files
-        for file in files:
-            # Check if the file is an image
-            if file.endswith(".tif"):
-                # Process the image in parallel
-                image_path = os.path.join(root, file)
-                pool.apply_async(resize_and_save_image, (image_path,))
+    args = parser.parse_args()
 
-    # Close the pool and wait for all processes to finish
-    pool.close()
-    pool.join()
+    target_size = (args.width, args.height)
 
+    resizer = ImageResizer(target_size, args.output_dir)
 
-# Resize and save images in the train directory
-process_images(train_dir)
+    # Resize and save images in the train directory
+    resizer.process_images(args.train_dir)
 
-# Resize and save images in the val directory
-process_images(val_dir)
+    # Resize and save images in the val directory
+    resizer.process_images(args.val_dir)
